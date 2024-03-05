29a3abcc022b0cec79379b348dd116782bba5fa2
Andrii Kurdiumov <kant2002@gmail.com>
2024-03-05 14:47:38 +0500

    Sample how to setup MSBuild in the same file.

---
+++ AppTasks/SimpleTask.cs
@@ -0,0 +1,16 @@
+﻿namespace AppTasks;
+
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+public class SimpleTask : Task
+{
+    public override bool Execute()
+    {
+        Log.LogMessage(MessageImportance.High, Message);
+        Log.LogMessage(MessageImportance.High, "Visit your local MSBuild community. Leave a task or two for those in need.");
+        return true;
+    }
+
+    public string Message { get; set; } = "The message to the gods of programming!";
+}
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # MSBuildTaskSameSolution
+
+Sample how to use MSBuild task within same solution.
+
+Most inconviniet in this setup is that you cannot modify MSBuild task without shutting down build server.
+Workaround for this is `RoslynCodeTaskFactory`, but this maybe not appropriate for everybody.
+
+If you decide keep tasks in same solution, learn to run `dotnet build-server shutdown` often.
